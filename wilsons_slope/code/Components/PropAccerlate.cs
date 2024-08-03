ba03dab7f5c9cc0284dd1ba3a5f42512d3e40aee
Anthony Tamargo <117713100+anthony-tamargo@users.noreply.github.com>
2024-08-02 20:01:17 -0700

    Added prop acceleration [SURFACE]
    Added multiple functions to the PropAccelerate component that check for Accel Type enum and apply acceleration to prop accordingly. Currently the SURFACE type is only supported but functionality is in place for adding AERIAL later.

--- wilsons_slope/code/Components/PropAccerlate.cs
+++ wilsons_slope/code/Components/PropAccerlate.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Net.Http.Headers;
 using Sandbox;
 
 public sealed class PropAccerlate : Component
 {
+	public enum PROP_ACCERATION_TYPE{
+		ARIEL, // prop will spawn in flying forward
+		SURFACE, // prop will drop to slope before accelerating
+	}
+	[Property]
+	public PROP_ACCERATION_TYPE propAccelType { get; set; }
+
 	Rigidbody rigidbody { get; set; }
 	protected override void OnAwake()
 	{
@@ -10,11 +18,61 @@ public sealed class PropAccerlate : Component
 	}
 	protected override void OnStart()
 	{
-		
 	}
 
 	protected override void OnUpdate()
 	{
+		AccelerateProp();
 		
+	}
+
+	private void AccelerateProp()
+	{
+		switch(propAccelType)
+		{
+			case PROP_ACCERATION_TYPE.SURFACE:
+				SurfaceAccelerate();
+			break;
+
+			case PROP_ACCERATION_TYPE.ARIEL:
+				ArielAccelerate();
+			break;
+		}
+	}
+
+	private void SurfaceAccelerate()
+	{
+		if(SurfaceGroundCheck())
+		{
+			rigidbody.Velocity = new Vector3(-750f, rigidbody.Velocity.y,rigidbody.Velocity.z);
+		}
+	}
+	private bool SurfaceGroundCheck()
+	{
+		var propPos = GameObject.Transform.Position;
+		var groundCheckRay = Scene.Trace
+			.Ray(propPos , propPos + (Vector3.Down * 15f) )
+			.WithTag("solid")
+			.Run();
+			if(groundCheckRay.Hit)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+	}
+	private void ArielAccelerate()
+	{
+
+	}
+	private void PropRotationLerp()
+	{
+		var randRot = new Angles(Game.Random.Float(-360f, 360f),Game.Random.Float(-360f, 360f), Game.Random.Float(-360f, 360f)) ;
+		var targetRot = randRot;
+		GameObject.Transform.Rotation = Angles.Lerp(GameObject.Transform.Rotation, targetRot , 5 * Time.Delta);
+
 	}
 }
